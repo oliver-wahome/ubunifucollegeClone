3d05c3c9add9fc9dbf8da2c0ee31246a0e6038f0
oliver-wahome <olivernderitu135@gmail.com>
2022-11-09 12:48:41 +0300

    Using javascript to create hamburger menu popup

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("hamburgerMenu").onclick = function(){
+    document.getElementById("workshops").style.display = "block";
+}
+
+document.getElementById("workshopCloseBtn").onclick = function() {
+    document.getElementById("workshops").style.display = "none";
+}
